596f03763cf55dbf058386a5da3e80ef619abf9f
Zan Lwine Htoo <zanlwine.htoo110@gmail.com>
2021-10-19 22:29:28 +0630

    first

--- Customer/src/main/java/com/example/Customer/CustomerApplication.java
+++ Customer/src/main/java/com/example/Customer/CustomerApplication.java
@@ -1,13 +1,29 @@
 package com.example.Customer;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CustomerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		final RestTemplate restTemplate = new RestTemplate();
+		return restTemplate;
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception{
+		return args -> {
+
+		};
 	}
 
 }
