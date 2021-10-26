86a3d2bcf85cd9913c89bdd2d3d49b529bb809bc
KurtReti <68139188+KurtReti@users.noreply.github.com>
2021-10-26 13:08:08 +1100

    Update CustomerApplication.java

--- Customer/src/main/java/com/example/Customer/CustomerApplication.java
+++ Customer/src/main/java/com/example/Customer/CustomerApplication.java
@@ -39,6 +39,58 @@ public class CustomerApplication {
 			ResponseEntity<Customer> postCustomer1 = restTemplate.postForEntity("http://localhost:8080/customers", customer1, Customer.class);
 			ResponseEntity<CustomerContact> postCustomerContact1 = restTemplate.postForEntity("http://localhost:8080/contacts", contact1, CustomerContact.class);
 			restTemplate.put("http://localhost:8080/customers/1/contacts/2",Customer.class);
+
+			Customer customer2 = new Customer();
+			customer2.setCompanyName("Apple");
+			customer2.setAddress("San Francisco");
+			customer2.setCountry("USA");
+			CustomerContact contact2 = new CustomerContact();
+			contact2.setName("Tim Cook");
+			contact2.setPhone("040513953");
+			contact2.setEmail("timapple@gmail.com");
+			contact2.setPosition("CEO");
+			ResponseEntity<Customer> postCustomer2 = restTemplate.postForEntity("http://localhost:8080/customers", customer2, Customer.class);
+			ResponseEntity<CustomerContact> postCustomerContact2 = restTemplate.postForEntity("http://localhost:8080/contacts", contact2, CustomerContact.class);
+			restTemplate.put("http://localhost:8080/customers/3/contacts/4");
+
+			Customer customer3 = new Customer();
+			customer3.setCompanyName("Cisco");
+			customer3.setAddress("San Francisco");
+			customer3.setCountry("USA");
+			CustomerContact contact3 = new CustomerContact();
+			contact3.setName("Chuck Robbins");
+			contact3.setPhone("040593102");
+			contact3.setEmail("crobbins@gmail.com");
+			contact3.setPosition("CEO");
+			ResponseEntity<Customer> postCustomer3 = restTemplate.postForEntity("http://localhost:8080/customers", customer3, Customer.class);
+			ResponseEntity<CustomerContact> postCustomerContact3 = restTemplate.postForEntity("http://localhost:8080/contacts", contact3, CustomerContact.class);
+			restTemplate.put("http://localhost:8080/customers/5/contacts/6");
+
+			Customer customer4 = new Customer();
+			customer4.setCompanyName("Microsoft");
+			customer4.setAddress("Redmond");
+			customer4.setCountry("USA");
+			CustomerContact contact4 = new CustomerContact();
+			contact4.setName("Steve Jobs");
+			contact4.setPhone("049182938");
+			contact4.setEmail("steve@gmail.com");
+			contact4.setPosition("CEO");
+			ResponseEntity<Customer> postCustomer4 = restTemplate.postForEntity("http://localhost:8080/customers", customer4, Customer.class);
+			ResponseEntity<CustomerContact> postCustomerContact4 = restTemplate.postForEntity("http://localhost:8080/contacts", contact4, CustomerContact.class);
+			restTemplate.put("http://localhost:8080/customers/7/contacts/8");
+
+			Customer customer5 = new Customer();
+			customer5.setCompanyName("Advanced Micro Devices");
+			customer5.setAddress("Santa Clara");
+			customer5.setCountry("USA");
+			CustomerContact contact5 = new CustomerContact();
+			contact5.setName("Lisa Su");
+			contact5.setPhone("049178271");
+			contact5.setEmail("lisasue@gmail.com");
+			contact5.setPosition("CEO");
+			ResponseEntity<Customer> postCustomer5 = restTemplate.postForEntity("http://localhost:8080/customers", customer5, Customer.class);
+			ResponseEntity<CustomerContact> postCustomerContact5 = restTemplate.postForEntity("http://localhost:8080/contacts", contact5, CustomerContact.class);
+			restTemplate.put("http://localhost:8080/customers/9/contacts/10");
 		};
 	}
 
